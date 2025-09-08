2ddd06a1f461173592817ab1869292a211336ac5
HitoKawiswara <hitokawiswara@gmail.com>
2025-09-08 16:39:15 +0700

    Commit No. 3 – use dynamic fragment

--- app/src/main/java/umn/ac/id/lab_week_03/MainActivity.kt
+++ app/src/main/java/umn/ac/id/lab_week_03/MainActivity.kt
@@ -4,26 +4,39 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 
 interface CoffeeListener {
     fun onSelected(id: Int)
 }
 
 class MainActivity : AppCompatActivity(), CoffeeListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
+
+        if (savedInstanceState == null) {
+            findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction()
+                    .add(containerLayout.id, listFragment)
+                    .commit()
+            }
+        }
     }
 
     override fun onSelected(id: Int) {
-        val detailFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_detail) as DetailFragment
-        detailFragment.setCoffeeData(id)
+        findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+            val detailFragment = DetailFragment.newInstance(id)
+            supportFragmentManager.beginTransaction()
+                .replace(containerLayout.id, detailFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
-    // Lifecycle logs
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
@@ -53,4 +66,3 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         private const val TAG = "MainActivity"
     }
 }
-
